e73b446774d28501f05675d0274aa026ca70fe91
MadeehaA6 <madea.anjum@gmail.com>
2022-04-22 20:12:04 -0600

    fix formatter

--- client/src/themes/theme.js
+++ client/src/themes/theme.js
@@ -1,52 +1,52 @@
-import { createTheme } from '@material-ui/core';
-import createBreakpoints from '@material-ui/core/styles/createBreakpoints';
+import { createTheme } from "@material-ui/core";
+import createBreakpoints from "@material-ui/core/styles/createBreakpoints";
 const breakpoints = createBreakpoints({});
 
 export const theme = createTheme({
   typography: {
-    fontFamily: 'Open Sans, sans-serif',
+    fontFamily: "Open Sans, sans-serif",
     fontSize: 14,
     fontWeightRegular: 400,
     button: {
-      textTransform: 'none',
+      textTransform: "none",
       letterSpacing: 0,
-      fontWeight: 'bold',
+      fontWeight: "bold",
     },
     h1: {
-      fontSize: '1.2rem',
-      lineHeight: '1.5',
-      [breakpoints.up('md')]: {
-        fontSize: '1.625rem',
+      fontSize: "1.2rem",
+      lineHeight: "1.5",
+      [breakpoints.up("md")]: {
+        fontSize: "1.625rem",
       },
-      [breakpoints.up('lg')]: {
-        fontSize: '2.25rem',
+      [breakpoints.up("lg")]: {
+        fontSize: "2.25rem",
       },
     },
     body1: {
-      fontSize: '14px',
+      fontSize: "14px",
     },
   },
 
   overrides: {
     MuiInput: {
       input: {
-        fontWeight: 'bold',
+        fontWeight: "bold",
       },
     },
   },
 
   palette: {
     text: {
-      primary: '#000000',
-      secondary: '#B0B0B0',
+      primary: "#000000",
+      secondary: "#B0B0B0",
     },
     primary: {
-      main: '#3A8DFF',
-      contrastText: '#ffffff',
+      main: "#3A8DFF",
+      contrastText: "#ffffff",
     },
     secondary: {
-      main: '#ffffff',
-      contrastText: '#3A8DFF',
+      main: "#ffffff",
+      contrastText: "#3A8DFF",
     },
   },
 });
